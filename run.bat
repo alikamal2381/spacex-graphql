a76d4965365ffaec16b3c6223d0db63ed75822d2
AliKamal <alikamal2381@gmail.com>
2020-10-03 15:47:29 +0500

    final-commit

---
+++ run.bat
@@ -0,0 +1,7 @@
+echo "# spacex-graphql" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/alikamal2381/spacex-graphql.git
+git push -u origin main
