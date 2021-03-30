3c5a481d26511b7e71633c46ea225bc177f0f908
AliKamal <alikamal2381@gmail.com>
2021-03-31 00:14:47 +0500

    sixth-commits

--- src/components/LaunchDet/app.tsx
+++ src/components/LaunchDet/app.tsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import { useLaunhDetailQuery } from '../../generated/graphql'
-import LaunchDet from './launchdet'
-import Loading from '../loading'
+import React from "react";
+import { useLaunhDetailQuery } from "../../generated/graphql";
+import LaunchDet from "./launchdet";
+import Loading from "../loading";
 
-
-// router 
-import { useParams } from 'react-router-dom'
+// router
+import { useParams } from "react-router-dom";
 
 const LaunchDetFunction = () => {
+  const { id } = useParams();
+  const { data, loading, error } = useLaunhDetailQuery({
+    variables: { id: id },
+  });
 
-    const { id } = useParams()
-    const { data, loading, error  } = useLaunhDetailQuery({variables : { id : id }})
+  if (loading) {
+    return <Loading />;
+  }
+  if (error || !data) {
+    console.log(error);
+    return <div>error</div>;
+  }
+  return <LaunchDet data={data} />;
+};
 
-    if (loading){
-        return <Loading />
-    }
-    if(error || !data){
-        console.log(error);
-        return <div>error</div>
-    }
-    return <LaunchDet data={data} />
-}
-
-
-export default LaunchDetFunction
+export default LaunchDetFunction;
